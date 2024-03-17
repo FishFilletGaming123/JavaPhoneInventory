eb49aa7226ef8cf5b58c6125d47edf3e7960bad9
FishFilletGaming123 <163716011+FishFilletGaming123@users.noreply.github.com>
2024-03-17 15:59:41 +0800

    Add files via upload

---
+++ src/main/java/com/mycompany/cellphone_inventory_record/Cellphone_Inventory_Record.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cellphone_inventory_record;
+
+/**
+ *
+ * @author user
+ */
+public class Cellphone_Inventory_Record {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
